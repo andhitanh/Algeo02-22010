48e665c460458088fa27f1aa96bd9a557f25bd71
Flora <mariaflorarenata12@gmail.com>
2023-11-14 18:13:23 +0700

    Optimize, added init

--- CBIRTekstur.py
+++ CBIRTekstur.py
@@ -1,33 +1,26 @@
-import cv2
 import numpy as np
-from PIL import Image
 import math
 import os.path
-#import matplotlib.pyplot as plt
-#from skimage.feature import graycomatrix, graycoprops
-#from skimage import data
 
 class Tekstur:
-    def CBIRTekstur(image,path):
+    def __init__(self,image,path):
+        self.image=image
+        self.path=path
+
+    def CBIRTekstur(self):
         #Cek ukuran gambar, resize jika terlalu besar
-        image=Tekstur.kompres(image,path)
+        self.image=Tekstur.kompres(self.image,self.path)
 
         #Konversi gambar ke numpy array
-        img=np.asarray(image)
+        img=np.asarray(self.image)
         row=np.shape(img)[0]
         col=np.shape(img)[1]
 
         #Konversi ke grayscale
         greyMat=np.zeros((np.shape(img)[0],np.shape(img)[1]),dtype=np.int32);
-        el=0
         for i in range(0,row):
             for j in range(0,col):
                 greyMat[i,j]=(img[i,j,0]*0.29)+(img[i,j,1]*0.587)+(img[i,j,2]*0.114)
-                el+=1
-                if (el%1000==0):
-                    print(el*100/(row*col),"%")
-        test = Image.fromarray(greyMat)
-        test.show("pleaseWork")
     
         #Pembuatan co-occurence matrix, d=1 dan offset=90
         occMat=np.zeros((256,256),dtype=np.int32)
@@ -47,11 +40,10 @@ class Tekstur:
                 CHEvec[1]+=occMat[i,j]/(1+((i-j)**2))
                 if (occMat[i,j]!=0):
                     CHEvec[2]+= -1*occMat[i,j]*(math.log(occMat[i,j]))
-        
         return CHEvec
                 
     def kompres(image,path):
-            while (os.path.getsize(path)>100000):
+            while (os.path.getsize(path)>50000):
                 image=image.resize((int(image.size[0]/2),int(image.size[1]/2)))
                 image.save(path, optimize=True, quality=25)
             return image
