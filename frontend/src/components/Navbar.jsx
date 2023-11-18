e10bfbd0b548e669e0ef16c3514b7485b27efc52
andhitanh <onecodhita@gmail.com>
2023-11-18 22:04:09 +0700

    feat fixing frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import logo from '../logo/bro.png'
+import logo2 from '../logo/sis.png'
 //import FaBars
 //import hamburger logo
 
@@ -7,14 +9,19 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#eff0f3] text-[#2a2a2a]">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#33272a] text-[#ffc6c7]">
         {/* navbar menu */}
         <div className="flex">
-          <ul className="flex" >
-            <li>Overview</li>
+          <ul className="font-bold flex" >
+            <li>Upload Your Pic!</li>
             <li>How to Use</li>
-            <li>About Us</li>
+            <li><Link to ="/about">About Us</Link></li>
+            
           </ul>
+
+          <img src={logo} alt="Logo" className="w-9 h-9 absolute right-10"/>
+          <img src={logo2} alt="Logo" className="w-11 h-10 absolute right-20"/>
+          
         </div>
         
         {/* hamburger */}
